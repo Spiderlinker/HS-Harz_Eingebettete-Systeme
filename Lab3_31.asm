9adc2c6a2f00f4c1e3c2d5d08f517b5260831a8c
Spiderlinker <u33873@hs-harz.de>
2020-06-22 14:44:36 +0200

    Aufgabe vervollstaendigt

--- Lab3_31.asm
+++ Lab3_31.asm
@@ -1,15 +1,55 @@
-NOP	                ; ## Aufgabe 1
+;RAHMEN.asm, Oliver Lindemann, u33873, Informatik, 22.06.2020
+;Praktikum 3, Aufgabe 1
+;-------------------------------------------------------------------------------------------------
+stack_seg segment para stack 'stack'    ;Stacksegment
+            db 100 dup(66h)             ;initialisiere 100 Byte mit Inhalt 66h, Stacklänge 100 Byte
+stack_seg ends
+;-------------------------------------------------------------------------------------------------
+data_seg segment 'data'                 ;Datensegment
+        datei db 'C:\text3.txt',00h     ;Dateipfad der Datei, die eingelesen werden soll.
+        handle dw 3333h                 ; Dateihandle beim Oeffnen der Datei erhalten
+        anzahl dw 4444h                 ; Anzahl gelesener Bytes
+        inhalt db 500 dup('$')          ;Reservierter Speicher für Inhalt der Datei
+        converted db 500 dup('$')       ; Konvertierter Inhalt der Datei
+
+data_seg ends
+;-------------------------------------------------------------------------------------------------
+prog_seg segment 'prog'                 ;Programmsegment
+assume 	cs:prog_seg, ds:data_seg, ss:stack_seg
+
+;-------------------------------------------------------------------------------------------------
+start:      mov ax,data_seg             ;Datensegmentadresse nach AX laden 
+            mov ds,ax                   ;Datensegmentadresse in Datensegmentregister DX laden
+
+;--------------------------------------Beginn eigener Code----------------------------------------	   
+
+        NOP	                    ; ## Aufgabe 1
+
+; -- Alternative mit 40h / 42h - Start
+;        MOV AH,3Ch              ; Create Handle
+;        LEA DX,datei            ; Dateipfad angeben
+;        MOV CX,0                ; Read only
+;        INT 21h                 ; Interrupt ausloesen, Handle erzeugen
+
+;        MOV handle,AX           ; Handle-Nummer speichern
+;        
+;        MOV AH,42h              ; Open-Handle
+;        MOV BX,handle           ; Handle-Nummer in BX fuer weitere Interrupts speichern
+;        MOV AL,0                ; Nur lesen
+;        INT 21h                 ; Interrupt ausloesen, Handle oeffnen
+; -- Alternative - Ende
+
 
         ; -- Datei oeffnen
         MOV AH,3dh              ; Open-Handle (Acess-Code)
         MOV AL,0                ; Open as read only
         LEA DX,datei            ; Dateipfad laden
         INT 21h                 ; Interrupt zum Oeffnen durchfuehren
-        MOV dateihandle,AX      ; Handle-Nummer in 'dateihandle' speichern
+        MOV handle,AX           ; Handle-Nummer in 'dateihandle' speichern
 
         ; -- Datei lesen
         MOV AH,3Fh
-        MOV BX,dateihandle      ; Handle(nummer) vom Oeffnen angeben
+        MOV BX,handle           ; Handle(nummer) vom Oeffnen angeben
         MOV CX,anzahl           ; Anzahl zu lesender Zeichen angeben
         LEA DX,inhalt           ; Speicherort der gelesenen Zeichen angeben
         INT 21h                 ; Datei lesen
@@ -18,7 +58,7 @@ NOP	                ; ## Aufgabe 1
 
         ; -- Datei schliessen
         MOV AH,3Eh              ; 3Eh - Code zum Schließen der Datei mit Interrupt 21h
-        MOV BX,dateihandle      ; Handle(nummer) angeben, die geschlossen werden soll
+        MOV BX,handle 		; Handle(nummer) angeben, die geschlossen werden soll
         INT 21h                 ; Datei schliessen	
 
         NOP                     ; ## Aufgabe 2
@@ -63,7 +103,7 @@ max:    LODSB                   ; naechstes Byte laden
         MOV BX,AX               ; Kein Sprung, AX ist groesser als aktuelles BX, weise BX aktuellen AX-Wert zu
 
 ; Sprungmarke, falls AX kleiner gleich des aktuell hoechsten Wertes ist
-belowEq:	DEC CX              ; Counter dekrementieren
+belowEq:    DEC CX              ; Counter dekrementieren
         JNZ max                 ; Wenn counter (CX) != 0 -> naechster Schleifendurchlauf	
 
         PUSH BX                 ; Hoechsten Wert auf Stack sichern
@@ -81,3 +121,11 @@ belowEq:	DEC CX              ; Counter dekrementieren
         MOV AH, 09h             ; Interrupt mitteilen, dass der gesamter String bis $ soll ausgegeben werden 
         MOV DX, offset converted; Daten für den Interrupt laden
         INT 21h                 ; Interrupt auslösen
+
+;---------------------------------------Ende eigener Code-----------------------------------------
+
+            mov ah,4ch           ; Funktion 4ch fuer int21h vorbereiten
+            int 21h              ; beendet das Programm und gibt die Kontrolle an das Betriebssystem zurueck
+prog_seg ends
+end start
+
